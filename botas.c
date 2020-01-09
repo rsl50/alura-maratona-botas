3f9c4cf8f706af539478c1f2f45edcd53f238e4a
Robson Lima <rsl.robson@gmail.com>
2020-01-09 14:38:57 -0300

    Commit inicial do desafio no SPOJ, ainda sem resposta no arquivo C

---
+++ botas.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LINE_SIZE	100
+
+int main () {
+	FILE *fp;
+	fp = freopen("input1.txt", "r", stdin);
+	
+	char buf[LINE_SIZE];
+	memset(buf, 0, LINE_SIZE);
+	
+	if (fp == NULL) {
+		printf("Erro ao abrir arquivo");
+		return (-1);
+	} else {
+		
+		while (fgets(buf, LINE_SIZE, fp) != NULL) {
+			
+		}
+	}
+	
+	fclose(fp);
+
+    return (0);
+}
